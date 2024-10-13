44f4e28960d3ec618d452524eccd1ccd044af7ed
William Nascimento <william08.dcc@gmail.com>
2024-10-13 02:56:02 -0300

    atv2-adc

--- project-atv1-gpio/main/main.c
+++ project-atv1-gpio/main/main.c
@@ -40,26 +40,57 @@ void app_main(void)
     gpio_reset_pin(BUTTON2_PIN);                         //reset pin and set as GPIO
     gpio_set_direction(BUTTON2_PIN, GPIO_MODE_INPUT);    //set BUTTON_PIN as input
     gpio_set_pull_mode(BUTTON2_PIN, GPIO_PULLUP_ONLY);   //set pull-up resistor
-
+    
+    // CONFIGURA O TIMER E CANAL PARA O LED3
+    ledc_timer_config_t timer_conf = {
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .timer_num = LEDC_TIMER_0,
+            .freq_hz = 1,
+            .duty_resolution = LEDC_TIMER_10_BIT,
+            .clk_cfg = LEDC_AUTO_CLK
+        };
+    ledc_timer_config(&timer_conf);
+    ledc_channel_config_t channel_config = {
+        .channel = LEDC_CHANNEL_1,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_sel = LEDC_TIMER_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = LED3_PIN,
+        .duty = 511
+    };    
+    ledc_channel_config(&channel_config);
+    
+    // DEFINICAO DE VARIAVEIS AUXILIARES
+    int led_status = 0; // define o estado do led1
     int led2_status = 0; // define o estado do led2
-    bool can_change = true; // permite ou n alterar o valor do led2
+    bool can_change = true; // permite ou n alterar o valor do led1
+    bool can_change2 = true; // permite ou n alterar o valor do led2
 
     while(true) //infinite loop
     {
+        // FAZ A LEITURA DOS BOTOES
         int new_state = gpio_get_level(BUTTON_PIN);   //read button state and save in new_state variable
         int new_state2 = gpio_get_level(BUTTON2_PIN);
         
-        if(new_state2 == 1 && can_change){
-            led2_status = (led2_status == 0) ? 1 : 0;
+        // VERIFICA SE TEM OU NAO QUE ALTERAR O ESTADO DO LED
+        if(new_state == 1 && can_change){
+            led_status = (led_status == 0) ? 1 : 0;
             can_change = false;
         }
-        if(new_state2 == 0){can_change = true;}
+        if(new_state == 0){can_change = true;}
+
+        if(new_state2 == 1 && can_change2){
+            led2_status = (led2_status == 0) ? 1 : 0;
+            can_change2 = false;
+        }
+        if(new_state2 == 0){can_change2 = true;}
         
-        gpio_set_level(LED1_PIN, new_state);
+        // SETA OS ESTADOS DOS LEDS
+        gpio_set_level(LED1_PIN, led_status);
         gpio_set_level(LED2_PIN, led2_status);
 
-        ESP_LOGI(TAG, "Bt1: %d | Bt2: %d |", new_state, new_state2);
+        ESP_LOGI(TAG, "Bt1: %d | Bt2: %d | LED1: %d | LED2: %d", new_state, new_state2, led_status, led2_status);
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);       // delay for 100 ms
+        vTaskDelay(10 / portTICK_PERIOD_MS);       // delay for 100 ms
     }
 }
